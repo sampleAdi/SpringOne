21315770d4717c548fe46c05dce88197d5472099
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-02 18:29:34 +0530

    Project uploaded till UC5

--- Test/src/main/java/com/example/Test/Controller/HelloRestController.java
+++ Test/src/main/java/com/example/Test/Controller/HelloRestController.java
@@ -1,7 +1,7 @@
 package com.example.Test.Controller;
 
 
-import org.springframework.web.bind.annotation .*;
+import org.springframework.web.bind.annotation.*;
 import com.example.Test.model.User;
 
 @RestController
@@ -15,17 +15,23 @@ public class HelloRestController {
     }
 
     @GetMapping("/query")
-    public String sayHello(@RequestParam String name){
-        return "Hello " + name+" from Bridgelabz";
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from Bridgelabz";
     }
 
     @GetMapping("/params/{name}")
-    public String sayHelloPathVariable(@PathVariable String name){
-        return "Hello " + name+" from Bridgelabz";
+    public String sayHelloPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from Bridgelabz";
     }
 
     @PostMapping("/post")
-    public String sayHelloPost(@RequestBody User user){
-        return "Hello " + user.getFirstName()+" "+user.getLastName()+" from Bridgelabz";
+    public String sayHelloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from Bridgelabz";
     }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+
 }
